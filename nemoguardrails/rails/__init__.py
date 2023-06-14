47d9a53dae7c7dfbe7f9b2e91b21a686083ff2c6
Traian Rebedea <trebedea@nvidia.com>
2023-06-14 14:56:30 +0300

    Changes for using NeMo LLM models using nvlangchain.

--- nemoguardrails/rails/__init__.py
+++ nemoguardrails/rails/__init__.py
@@ -13,5 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from nvlangchain.apis import service
+from nvlangchain.llm import NeMo
+
 from .llm.config import RailsConfig
 from .llm.llmrails import LLMRails
+
+
+# Monkey patching to enable async call, even though in a blocking way.
+async def _acall(self, prompt, stop):
+    return self._call(prompt, stop)
+
+
+NeMo._acall = _acall
+
+# Quick hackish way to connect to staging
+# TODO: Is there a better way?
+service.API_HOST = "https://api.stg.llm.ngc.nvidia.com/v1"
