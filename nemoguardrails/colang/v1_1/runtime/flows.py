6c62b49f4e39793cb3b8583ab809f748d4ef661d
Christian Schüller <cschueller@nvidia.com>
2023-09-12 20:35:27 +0200

    Fix potential issues with state serialization

--- nemoguardrails/colang/v1_1/runtime/flows.py
+++ nemoguardrails/colang/v1_1/runtime/flows.py
@@ -172,12 +172,12 @@ class Action:
 
         # The action event name mapping
         self._event_name_map = {
-            "Start": self.start,
-            "Change": self.change,
-            "Stop": self.stop,
-            "Started": self.started_event,
-            "Updated": self.updated_event,
-            "Finished": self.finished_event,
+            "Start": "start",
+            "Change": "change",
+            "Stop": "stop",
+            "Started": "started_event",
+            "Updated": "updated_event",
+            "Finished": "finished_event",
         }
 
     # Process an event
@@ -195,7 +195,9 @@ class Action:
 
     def get_event(self, name: str, arguments: dict) -> Callable[[], ActionEvent]:
         """Returns the corresponding action event."""
-        return self._event_name_map[name](arguments)
+        assert name in self._event_name_map, f"Event '{name}' not available!"
+        func = getattr(self, self._event_name_map[name])
+        return func(arguments)
 
     # Action events to send
     def start(self, args: dict) -> ActionEvent:
@@ -364,20 +366,22 @@ class FlowState:
 
     def __post_init__(self) -> None:
         self._event_name_map = {
-            "Start": self.start,
-            "Stop": self.stop,
-            "Pause": self.pause,
-            "Resume": self.resume,
-            "Started": self.started_event,
-            "Paused": self.paused_event,
-            "Resumed": self.resumed_event,
-            "Finished": self.finished_event,
-            "Failed": self.failed_event,
+            "Start": "start",
+            "Stop": "stop",
+            "Pause": "pause",
+            "Resume": "resume",
+            "Started": "started_event",
+            "Paused": "paused_event",
+            "Resumed": "resumed_event",
+            "Finished": "finished_event",
+            "Failed": "failed_event",
         }
 
     def get_event(self, name: str, arguments: dict) -> Callable[[], FlowEvent]:
         """Returns the corresponding action event."""
-        return self._event_name_map[name](arguments)
+        assert name in self._event_name_map, f"Event '{name}' not available!"
+        func = getattr(self, self._event_name_map[name])
+        return func(arguments)
 
     # Flow events to send
     def start(self, args: dict) -> FlowEvent:
