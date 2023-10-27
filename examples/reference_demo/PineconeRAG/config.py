b2b7fa3d7ba6310c3b00e7e8f838759868fdb13f
Siddha Ganju <sganju@nvidia.com>
2023-10-26 22:22:12 -0700

    using embedding model from config
    Signed-off-by: Siddha Ganju <sganju@nvidia.com>

--- examples/reference_demo/PineconeRAG/config.py
+++ examples/reference_demo/PineconeRAG/config.py
@@ -16,38 +16,22 @@ import logging
 import os
 import time
 from datetime import datetime
-from pprint import pprint
-from typing import List, Optional
-from uuid import uuid4
+from typing import Optional
 
-import fitz
 import pinecone
-import tiktoken
-from datasets import Dataset
-from langchain.chains import RetrievalQA, RetrievalQAWithSourcesChain
-from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import BaseLLM
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
-from tqdm.auto import tqdm
-from pprint import pprint
 from nemoguardrails import LLMRails
 from nemoguardrails.actions import action
 from nemoguardrails.actions.actions import ActionResult
-from nemoguardrails.embeddings.basic import OpenAIEmbeddingModel
-from nemoguardrails.embeddings.index import EmbeddingsIndex, IndexItem
+from nemoguardrails.llm.taskmanager import LLMTaskManager
 
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
 PINECONE_ENVIRONMENT = os.environ.get("PINECONE_ENVIRONMENT")
 index_name = "nemoguardrailsindex"
-model_name = "text-embedding-ada-002"
-# todo different llm for different tasks
-# https://github.com/NVIDIA/NeMo-Guardrails/blob/aa07d889e9437dc687cd1c0acf51678ad435516e/tests/test_configs/with_openai_embeddings/config.yml#L4
-
-# import warnings
-# warnings.filterwarnings("ignore")
 
 LOG_FILENAME = datetime.now().strftime("logs/mylogfile_%H_%M_%d_%m_%Y.log")
 log = logging.getLogger(__name__)
@@ -56,29 +40,23 @@ logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 
 @action(is_system_action=True)
 async def answer_question_with_sources(
-    context: Optional[dict] = None, llm: Optional[BaseLLM] = None
+    llm_task_manager: LLMTaskManager,
+    context: Optional[dict] = None,
+    llm: Optional[BaseLLM] = None
 ):
     """Retrieve relevant chunks from the knowledge base and add them to the context."""
     user_message = context.get("last_user_message")
     text_field = "text"
-    # switch back to normal index for langchain
-
-    embed = OpenAIEmbeddings(model=model_name, openai_api_key=OPENAI_API_KEY)
-    vectorstore = Pinecone(pinecone.Index(index_name), embed.embed_query, text_field)
-    # vectorstore = Pinecone(pinecone.Index(index_name), OpenAIEmbeddingModel.embed_query, text_field)
-
-    '''
-    qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
-    )
-    '''
-
-    
+    embed = OpenAIEmbeddings(model=[model.model for model in llm_task_manager.config.models if model.type == "embeddings"][0], 
+                             openai_api_key=OPENAI_API_KEY)
+    vectorstore = Pinecone(pinecone.Index(index_name),
+                           embed.embed_query, text_field)
     qa_with_sources = RetrievalQA.from_chain_type(
-        llm=llm, chain_type="stuff", 
-        retriever=vectorstore.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 30}), 
+        llm=llm, chain_type="stuff",
+        retriever=vectorstore.as_retriever(
+            search_type="mmr", search_kwargs={'fetch_k': 30}),
         return_source_documents=True)
-    
+
     start_time = time.time()
     result = qa_with_sources(user_message)
     print(
@@ -107,7 +85,6 @@ async def answer_question_with_sources(
         return_value=context_updates["relevant_chunks"],
         context_updates=context_updates,
     )
-
-
 def init(app: LLMRails):
-    app.register_action(answer_question_with_sources, "answer_question_with_sources")
+    app.register_action(answer_question_with_sources,
+                        "answer_question_with_sources")
