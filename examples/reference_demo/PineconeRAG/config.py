4d907a08851f88834af842c97f297d58e3525ee3
Siddha Ganju <sganju@nvidia.com>
2023-10-26 21:37:58 -0700

    added relevant chunks

--- examples/reference_demo/PineconeRAG/config.py
+++ examples/reference_demo/PineconeRAG/config.py
@@ -31,7 +31,7 @@ from langchain.llms import BaseLLM
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Pinecone
 from tqdm.auto import tqdm
-
+from pprint import pprint
 from nemoguardrails import LLMRails
 from nemoguardrails.actions import action
 from nemoguardrails.actions.actions import ActionResult
@@ -67,36 +67,42 @@ async def answer_question_with_sources(
     vectorstore = Pinecone(pinecone.Index(index_name), embed.embed_query, text_field)
     # vectorstore = Pinecone(pinecone.Index(index_name), OpenAIEmbeddingModel.embed_query, text_field)
 
+    '''
     qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
         llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
     )
+    '''
+
+    
+    qa_with_sources = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", 
+        retriever=vectorstore.as_retriever(search_type="mmr", search_kwargs={'fetch_k': 30}), 
+        return_source_documents=True)
+    
     start_time = time.time()
-    result = qa_with_sources(context.get("last_user_message"))
+    result = qa_with_sources(user_message)
     print(
         "Getting the answer back from pinecone took: ",
         time.time() - start_time,
         " seconds!",
     )
-    answer = result["answer"]
-    source_ref = str(result["sources"])
-
+    answer = result["result"]
+    source_ref = str(result["source_documents"])
+    '''
+    Note: Relevant chunks are stored in source_ref with the following format, 
+    and urls are already contained inside the metadata tag of source_ref
+    {'query': '',
+    'result': '',
+    'source_documents': [Document(page_content='...', 
+    metadata={'chunk': int, 'id': 'int', 'source': '/path/to/file.pdf'}),
+    ]
+    }
+    '''
     context_updates = {
         "relevant_chunks": source_ref,
         "user_question": user_message,
         "bot_response": answer,
     }
-
-    '''
-    context_updates = {
-        "relevant_chunks": f"""
-                Question: {user_message}
-                Answer: {answer},
-                Citing: {"None--"}
-                Source : {source_ref}
-        """
-    }
-    '''
-
     return ActionResult(
         return_value=context_updates["relevant_chunks"],
         context_updates=context_updates,
