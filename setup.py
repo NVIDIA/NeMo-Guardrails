bc7e0f804a6623fd849b2af6b1c749b6d21d07ef
Bogdan Raduta <bogdan.raduta@flowx.ai>
2023-12-05 17:10:38 +0200

    modified setup

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="nemoguardrails",
-    version="0.7.2-rc1",
+    version="0.6.0-rc2",
     packages=find_packages(),
     author="NVIDIA",
     author_email="nemoguardrails@nvidia.com",
@@ -46,7 +46,6 @@ setup(
     entry_points={
         "console_scripts": ["nemoguardrails=nemoguardrails.__main__:app"],
     },
-    include_package_data=True,
     package_data={
         "nemoguardrails": [
             "**/*.yml",
