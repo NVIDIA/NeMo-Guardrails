9bfebb2abad3e37297547031059d7d2a1d6f9980
Erick Galinkin <egalinkin@nvidia.com>
2024-01-08 15:32:18 -0500

    Add jailbreak heuristic code for length/perplexity check.

---
+++ nemoguardrails/library/jailbreak_detection/heuristics/checks.py
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+
+THRESH = 89.79  # This obviously needs to be more easily configurable
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_id = "gpt2-large"
+model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
+
+
+def get_perplexity(input_string: str) -> bool:
+    encodings = tokenizer(input_string, return_tensors="pt")
+
+    max_length = model.config.n_positions
+    stride = 512
+    seq_len = encodings.input_ids.size(1)
+
+    nlls = list()
+    prev_end_loc = 0
+    for begin_loc in range(0, seq_len, stride):
+        end_loc = min(begin_loc + max_length, seq_len)
+        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+        input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
+        target_ids = input_ids.clone()
+        target_ids[:, :-trg_len] = -100
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+            neg_log_likelihood = outputs.loss
+
+        nlls.append(neg_log_likelihood)
+
+        prev_end_loc = end_loc
+        if end_loc == seq_len:
+            break
+
+    perplexity = torch.exp(torch.stack(nlls).mean())
+
+    return perplexity.cpu().detach().numpy().item()
+
+
+def check_jb_lp(input_string: str) -> dict:
+    print(input_string)
+    perplexity = get_perplexity(input_string)
+    jb_lp = len(input_string) / perplexity >= THRESH
+    result = {"jailbreak": jb_lp}
+    return result
---
+++ nemoguardrails/library/jailbreak_detection/server.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+import typer
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from heuristics import checks
+
+app = FastAPI()
+cli_app = typer.Typer()
+
+device = os.environ.get("JAILBREAK_CHECK_DEVICE", "cpu")
+
+
+class JailbreakCheckRequest(BaseModel):
+    prompt: str
+
+
+@app.get("/")
+def hello_world():
+    welcome_str = (
+        f"This is a development server for jailbreak detection.\n"
+        f"<br>Hit the /heuristics endpoint to run all heuristics by sending a POST request with the user prompt."
+        f"<br>Example: curl -X POST -d 'prompt=This is a user prompt' http://localhost:1337/heuristics\n"
+        f"Detailed documentation and all endpoints are included in the README."
+    )
+    return welcome_str
+
+
+@app.post("/jailbreak_lp_heuristic")
+def lp_heuristic_check(request: JailbreakCheckRequest):
+    return checks.check_jb_lp(request.prompt)
+
+
+@app.post("/heuristics")
+def run_all_heuristics(request: JailbreakCheckRequest):
+    # Will add other heuristics as they become available
+    lp_check = checks.check_jb_lp(request.prompt)
+    jailbreak = any([lp_check["jailbreak"]])
+    heuristic_checks = {
+        "jailbreak": jailbreak,
+        "length_perplexity": lp_check["jailbreak"],
+    }
+    return heuristic_checks
+
+
+@cli_app.command()
+def start(
+    port: int = typer.Option(
+        default=1337, help="The port that the server should listen on."
+    ),
+):
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    cli_app()
