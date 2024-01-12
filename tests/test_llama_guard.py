bc5eb55b61839701dadde0d3dc82bcb65edaf1c6
Prasoon Varshney <prasoonv@nvidia.com>
2024-01-12 00:37:44 -0500

    Add tests for Llama Guard
    Signed-off-by: Prasoon Varshney <prasoonv@nvidia.com>

---
+++ tests/test_llama_guard.py
@@ -0,0 +1,217 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from nemoguardrails import LLMRails, RailsConfig
+from nemoguardrails.actions.actions import ActionResult
+from tests.utils import FakeLLM, TestChat
+
+COLANG_CONFIG = """
+define user express greeting
+  "hi"
+
+define bot refuse to respond
+  "I'm sorry, I can't respond to that."
+
+define flow llama guard check input
+  $llama_guard_response = execute llama_guard_check_input
+  $allowed = $llama_guard_response["allowed"]
+  $llama_guard_policy_violations = $llama_guard_response["policy_violations"]
+
+  if not $allowed
+    bot refuse to respond
+    stop
+
+define flow llama guard check output
+  $llama_guard_response = execute llama_guard_check_output
+  $allowed = $llama_guard_response["allowed"]
+  $llama_guard_policy_violations = $llama_guard_response["policy_violations"]
+
+  if not $allowed
+    bot refuse to respond
+    stop
+"""
+
+YAML_CONFIG = """
+models:
+  - type: main
+    engine: openai
+    model: gpt-3.5-turbo-instruct
+
+  - type: llama_guard
+    engine: vllm_openai
+    parameters:
+      openai_api_base: "http://localhost:5000/v1"
+      model_name: "meta-llama/LlamaGuard-7b"
+
+rails:
+  input:
+    flows:
+      - llama guard check input
+
+  output:
+    flows:
+      - llama guard check output
+
+prompts:
+  - task: llama_guard_check_input
+    content: |
+      <s>[INST] Sample content. Only the entry needs to exist for this test. [/INST]
+
+  - task: llama_guard_check_output
+    content: |
+      <s>[INST] Sample content. Only the entry needs to exist for this test. [/INST]
+"""
+
+
+def test_llama_guard_check_all_safe():
+    """
+    Test the chat flow when both llama_guard_check_input and llama_guard_check_output actions return "safe"
+    """
+    config = RailsConfig.from_content(
+        colang_content=COLANG_CONFIG, yaml_content=YAML_CONFIG
+    )
+    chat = TestChat(
+        config,
+        llm_completions=[
+            "Mock generated user intent",  # mock response for the generate_user_intent action
+            "Mock generated next step",  # mock response for the generate_next_step action
+            "  Hi there! How are you doing?",  # mock response for the generate_bot_message action
+        ],
+    )
+
+    llama_guard_llm = FakeLLM(
+        responses=[
+            "safe",  # llama_guard_check_input
+            "safe",  # llama_guard_check_output
+        ]
+    )
+    chat.app.register_action_param("llama_guard_llm", llama_guard_llm)
+
+    chat >> "Hi"
+    chat << "Hi there! How are you doing?"
+
+
+def test_llama_guard_check_input_unsafe():
+    """
+    Test the chat flow when the llama_guard_check_input action returns "unsafe"
+    """
+    config = RailsConfig.from_content(
+        colang_content=COLANG_CONFIG, yaml_content=YAML_CONFIG
+    )
+    chat = TestChat(
+        config,
+        llm_completions=[
+            # Since input is unsafe, the main llm doesn't need to perform any of
+            # generate_user_intent, generate_next_step, or generate_bot_message
+            # Dev note: iff the input was safe, this empty llm_completions list would result in a test failure.
+        ],
+    )
+
+    llama_guard_llm = FakeLLM(
+        responses=[
+            "unsafe",  # llama_guard_check_input
+            # "safe",  # commented out to assert that the flow never reaches llama_guard_check_output (since input check failed)
+        ]
+    )
+    chat.app.register_action_param("llama_guard_llm", llama_guard_llm)
+
+    chat >> "Unsafe input"
+    chat << "I'm sorry, I can't respond to that."
+
+
+def test_llama_guard_check_input_error():
+    """
+    Test the chat flow when the llama_guard_check_input action raises an error
+    """
+    config = RailsConfig.from_content(
+        colang_content=COLANG_CONFIG, yaml_content=YAML_CONFIG
+    )
+    chat = TestChat(
+        config,
+        llm_completions=[
+            # Since input is unsafe, the main llm doesn't need to perform any of
+            # generate_user_intent, generate_next_step, or generate_bot_message
+            # Dev note: iff the input was safe, this empty llm_completions list would result in a test failure.
+        ],
+    )
+
+    llama_guard_llm = FakeLLM(
+        responses=[
+            "error",  # llama_guard_check_input
+            # "safe",  # commented out to assert that the flow never reaches llama_guard_check_output (since input check failed)
+        ]
+    )
+    chat.app.register_action_param("llama_guard_llm", llama_guard_llm)
+
+    chat >> "Unsafe input"
+    chat << "I'm sorry, I can't respond to that."
+
+
+def test_llama_guard_check_output_unsafe():
+    """
+    Test the chat flow when the llama_guard_check_input action raises an error
+    """
+    config = RailsConfig.from_content(
+        colang_content=COLANG_CONFIG, yaml_content=YAML_CONFIG
+    )
+    chat = TestChat(
+        config,
+        llm_completions=[
+            "Mock generated user intent",  # mock response for the generate_user_intent action
+            "Mock generated next step",  # mock response for the generate_next_step action
+            "  Hi there! How are you doing?",  # mock response for the generate_bot_message action
+        ],
+    )
+
+    llama_guard_llm = FakeLLM(
+        responses=[
+            "safe",  # llama_guard_check_input
+            "unsafe",  # llama_guard_check_output
+        ]
+    )
+    chat.app.register_action_param("llama_guard_llm", llama_guard_llm)
+
+    chat >> "Unsafe input"
+    chat << "I'm sorry, I can't respond to that."
+
+
+def test_llama_guard_check_output_error():
+    """
+    Test the chat flow when the llama_guard_check_input action raises an error
+    """
+    config = RailsConfig.from_content(
+        colang_content=COLANG_CONFIG, yaml_content=YAML_CONFIG
+    )
+    chat = TestChat(
+        config,
+        llm_completions=[
+            "Mock generated user intent",  # mock response for the generate_user_intent action
+            "Mock generated next step",  # mock response for the generate_next_step action
+            "  Hi there! How are you doing?",  # mock response for the generate_bot_message action
+        ],
+    )
+
+    llama_guard_llm = FakeLLM(
+        responses=[
+            "safe",  # llama_guard_check_input
+            "error",  # llama_guard_check_output
+        ]
+    )
+    chat.app.register_action_param("llama_guard_llm", llama_guard_llm)
+
+    chat >> "Unsafe input"
+    chat << "I'm sorry, I can't respond to that."
