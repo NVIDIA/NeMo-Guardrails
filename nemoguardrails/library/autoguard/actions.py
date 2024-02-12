80456cf341b81e947bb0de00b4d20f5f346b01db
abhijitpal1247 <abhijit@samasra.net>
2024-02-12 12:18:14 +0530

    added pii and topical guardrails
    Signed-off-by: abhijitpal1247 <abhijit@samasra.net>

--- nemoguardrails/library/autoguard/actions.py
+++ nemoguardrails/library/autoguard/actions.py
@@ -28,7 +28,6 @@ from nemoguardrails.llm.taskmanager import LLMTaskManager
 log = logging.getLogger(__name__)
 
 GUARDRAIL_TRIGGER_TEXT = {
-    "pii_fast": "PII",
     "confidential_detection": "Confidential Information violation",
     "gender_bias_detection": "Gender bias in text",
     "harm_detection": "Harm to human violation",
@@ -39,6 +38,43 @@ GUARDRAIL_TRIGGER_TEXT = {
     "factcheck": "Factcheck violation in text",
 }
 
+DEFAULT_CONFIG = {
+    "pii_fast": {
+        "mode": "OFF",
+        "mask": True,
+        "enabled_types": [
+            "[PERSON NAME]",
+            "[LOCATION]",
+            "[DATE OF BIRTH]",
+            "[DATE]",
+            "[PHONE NUMBER]",
+            "[EMAIL ADDRESS]",
+            "[CREDIT CARD NUMBER]",
+            "[BANK ACCOUNT NUMBER]",
+            "[SOCIAL SECURITY NUMBER]",
+            "[MONEY]",
+            "[INSURANCE POLICY NUMBER]",
+            "[PROFESSION]",
+            "[ORGANIZATION]",
+            "[USERNAME]",
+            "[PASSWORD]",
+            "[IP ADDRESS]",
+            "[PASSPORT NUMBER]",
+            "[DRIVER LICENSE NUMBER]",
+            "[API_KEY]",
+            "[TRANSACTION_ID]",
+        ],
+    },
+    "confidential_detection": {"mode": "OFF"},
+    "gender_bias_detection": {"mode": "OFF"},
+    "harm_detection": {"mode": "OFF"},
+    "text_toxicity_extraction": {"mode": "OFF"},
+    "racial_bias_detection": {"mode": "OFF"},
+    "tonal_detection": {"mode": "OFF"},
+    "jailbreak_detection": {"mode": "OFF"},
+    "intellectual_property": {"mode": "OFF"},
+}
+
 
 async def autoguard_infer(request_url, text, tasks, task_config=None):
     api_key = os.environ.get("AUTOGUARD_API_KEY")
@@ -46,50 +82,13 @@ async def autoguard_infer(request_url, text, tasks, task_config=None):
         raise ValueError("AUTOGUARD_API_KEY environment variable not set.")
 
     headers = {"x-api-key": api_key}
-    # shutting all guardrails off by default
-    config = {
-        "pii_fast": {
-            "mode": "OFF",
-            "mask": True,
-            "enabled_types": [
-                "[PERSON NAME]",
-                "[LOCATION]",
-                "[DATE OF BIRTH]",
-                "[DATE]",
-                "[PHONE NUMBER]",
-                "[EMAIL ADDRESS]",
-                "[CREDIT CARD NUMBER]",
-                "[BANK ACCOUNT NUMBER]",
-                "[SOCIAL SECURITY NUMBER]",
-                "[MONEY]",
-                "[INSURANCE POLICY NUMBER]",
-                "[PROFESSION]",
-                "[ORGANIZATION]",
-                "[USERNAME]",
-                "[PASSWORD]",
-                "[IP ADDRESS]",
-                "[PASSPORT NUMBER]",
-                "[DRIVER LICENSE NUMBER]",
-                "[API_KEY]",
-                "[TRANSACTION_ID]",
-            ],
-        },
-        "confidential_detection": {"mode": "OFF"},
-        "gender_bias_detection": {"mode": "OFF"},
-        "harm_detection": {"mode": "OFF"},
-        "text_toxicity_extraction": {"mode": "OFF"},
-        "racial_bias_detection": {"mode": "OFF"},
-        "tonal_detection": {"mode": "OFF"},
-        "jailbreak_detection": {"mode": "OFF"},
-        "intellectual_property": {"mode": "OFF"},
-    }
+    config = DEFAULT_CONFIG
     # enable the select guardrail
     for task in tasks:
         config[task] = {"mode": "DETECT"}
         if task_config:
             config[task].update(task_config)
     request_body = {"prompt": text, "config": config}
-    aggregated_responses = []
 
     async with aiohttp.ClientSession() as session:
         async with session.post(
@@ -108,6 +107,42 @@ async def autoguard_infer(request_url, text, tasks, task_config=None):
                     resp = json.loads(line_text)
                     if resp["guarded"]:
                         return True, GUARDRAIL_TRIGGER_TEXT[resp["task"]]
+    return False, None
+
+
+async def autoguard_pii_infer(request_url, text, entities, task_config=None):
+    api_key = os.environ.get("AUTOGUARD_API_KEY")
+    if api_key is None:
+        raise ValueError("AUTOGUARD_API_KEY environment variable not set.")
+
+    headers = {"x-api-key": api_key}
+    config = DEFAULT_CONFIG
+    # enable the select guardrail
+    config["pii_fast"] = {"mode": "DETECT"}
+    if task_config:
+        config["pii_fast"].update(task_config)
+
+    config["pii_fast"]["enabled_types"] = entities
+
+    request_body = {"prompt": text, "config": config}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=request_url,
+            headers=headers,
+            json=request_body,
+        ) as response:
+            if response.status != 200:
+                raise ValueError(
+                    f"AutoGuard call failed with status code {response.status}.\n"
+                    f"Details: {await response.text()}"
+                )
+            async for line in response.content:
+                line_text = line.strip()
+                if len(line_text) > 0:
+                    resp = json.loads(line_text)
+                    if resp["task"] == "pii_fast":
+                        return resp["guarded"], resp["response"]
     return False, None
 
 
@@ -130,10 +165,36 @@ async def autoguard_factcheck_infer(request_url, text, documents):
                     f"Details: {await response.text()}"
                 )
             async for line in response.content:
-                response = json.loads(line)
-                if response["task"] == "factcheck":
-                    return float(response["response"][17:])
+                resp = json.loads(line)
+                if resp["task"] == "factcheck":
+                    return float(resp["response"][17:])
     return 1.0
+
+
+async def autoguard_topical_infer(request_url, text, completion):
+    api_key = os.environ.get("AUTOGUARD_API_KEY")
+    if api_key is None:
+        raise ValueError("AUTOGUARD_API_KEY environment variable not set.")
+
+    headers = {"x-api-key": api_key}
+    request_body = {"prompt": text, "completion": completion}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url=request_url,
+            headers=headers,
+            json=request_body,
+        ) as response:
+            if response.status != 200:
+                raise ValueError(
+                    f"AutoGuard call failed with status code {response.status}.\n"
+                    f"Details: {await response.text()}"
+                )
+            async for line in response.content:
+                resp = json.loads(line)
+                print(resp)
+                if resp["task"] == "topical_detection":
+                    return resp["guarded"]
+    return False
 
 
 @action()
@@ -149,10 +210,24 @@ async def autoguard_input_api(
     tasks = getattr(autoguard_config.input, "guardrails")
     if not tasks:
         raise ValueError("Provide the guardrails in the config")
-
     prompt = user_message
 
     return await autoguard_infer(autoguard_api_url, prompt, tasks)
+
+
+@action()
+async def autoguard_pii_api(
+    llm_task_manager: LLMTaskManager, context: Optional[dict] = None
+):
+    user_message = context.get("user_message")
+    autoguard_config = llm_task_manager.config.rails.config.autoguard
+
+    autoguard_api_url = autoguard_config.parameters.get("endpoint")
+    if not autoguard_api_url:
+        raise ValueError("Provide the autoguard endpoint in the config")
+
+    entities = getattr(autoguard_config, "entities", [])
+    return await autoguard_pii_infer(autoguard_api_url, user_message, entities)
 
 
 @action()
@@ -174,20 +249,20 @@ async def autoguard_output_api(
 
 
 @action()
-async def autoguard_factcheck_api(
+async def autoguard_topical_api(
     llm_task_manager: LLMTaskManager, context: Optional[dict] = None
 ):
+    user_message = context.get("user_message")
     bot_message = context.get("bot_message")
-    documents = context.get("relevant_chunks", [])
+    print(user_message)
+    print(bot_message)
     autoguard_config = llm_task_manager.config.rails.config.autoguard
-    autoguard_api_url = autoguard_config.parameters.get("fact_check_endpoint")
-    if not autoguard_api_url:
-        raise ValueError("Provide the autoguard factcheck endpoint in the config")
-    if isinstance(documents, str):
-        documents = documents.split("\n")
-    prompt = bot_message
+    autoguard_topical_api_url = autoguard_config.parameters.get(
+        "autoguard_topical_endpoint"
+    )
+    if not autoguard_topical_api_url:
+        raise ValueError("Provide the autoguard topical endpoint in the config")
 
-    if isinstance(documents, list) and len(documents) > 0:
-        return await autoguard_factcheck_infer(autoguard_api_url, prompt, documents)
-    else:
-        raise ValueError("Provide relevant documents in proper format")
+    return await autoguard_topical_infer(
+        autoguard_topical_api_url, user_message, bot_message
+    )
