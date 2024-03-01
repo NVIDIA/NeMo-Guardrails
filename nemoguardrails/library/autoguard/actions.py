1b1a5123f858b96cec9fc92253219646b7edbb44
abhijitpal1247 <abhijit@samasra.net>
2024-03-01 19:01:42 +0530

    changed PII implementation and some tests

--- nemoguardrails/library/autoguard/actions.py
+++ nemoguardrails/library/autoguard/actions.py
@@ -21,6 +21,8 @@ from typing import Any, Dict, List, Optional
 import aiohttp
 
 from nemoguardrails.actions import action
+from nemoguardrails.actions.actions import ActionResult
+from nemoguardrails.kb.kb import KnowledgeBase
 from nemoguardrails.llm.taskmanager import LLMTaskManager
 
 log = logging.getLogger(__name__)
@@ -34,14 +36,12 @@ GUARDRAIL_RESPONSE_TEXT = {
     "racial_bias_detection": "Racial bias",
     "jailbreak_detection": "Jailbreak attempt",
     "intellectual_property": "Intellectual property",
-    "factcheck": "Factcheck violation",
 }
 
 DEFAULT_CONFIG = {
     "pii_fast": {
         "mode": "OFF",
         "mask": False,
-        "coreference": False,
         "enabled_types": [
             "[BANK ACCOUNT NUMBER]",
             "[CREDIT CARD NUMBER]",
@@ -83,25 +83,32 @@ def process_autoguard_output(responses: List[Any]):
 
     prefix = []
     suffix = []
+    pii_response = ""
+    output_str = ""
+    suffix_str = ""
     for response in responses:
         if response["task"] == "text_toxicity_extraction":
             suffix += response["output_data"]
-        prefix += [GUARDRAIL_RESPONSE_TEXT[response["task"]]]
 
-    output_str = (
-        ", ".join(prefix) + " has been detected by AutoGuard; Sorry, can't process."
-    )
-    suffix_str = ""
+        if response["task"] == "pii_fast":
+            pii_response = response["response"]
+        else:
+            prefix += [GUARDRAIL_RESPONSE_TEXT[response["task"]]]
+
+    if len(prefix) > 0:
+        output_str = (
+            ", ".join(prefix) + " has been detected by AutoGuard; Sorry, can't process."
+        )
     if len(suffix) > 0:
         suffix_str += " Toxic phrases: " + ", ".join(suffix)
-    return output_str, suffix_str
+    if len(pii_response) > 0:
+        output_str = pii_response + "\n" + output_str
+    return [output_str, suffix_str]
 
 
 async def autoguard_infer(
     request_url: str,
     text: str,
-    tasks: List[str],
-    matching_scores: Dict[str, Dict[str, float]],
     task_config: Optional[Dict[Any, Any]] = None,
 ):
     """Checks whether the given text passes through the applied guardrails."""
@@ -112,14 +119,14 @@ async def autoguard_infer(
     headers = {"x-api-key": api_key}
     config = DEFAULT_CONFIG
     # enable the select guardrail
-    for task in tasks:
-        if task not in ["pii_fast", "factcheck"]:
-            config[task] = {"mode": "DETECT"}
-            if matching_scores:
-                config[task]["matching_scores"] = matching_scores.get(task, {})
-        if task_config:
-            config[task].update(task_config)
+    for task in task_config.keys():
+        if task != "factcheck":
+            config[task]["mode"] = "DETECT"
+        if task_config[task]:
+            config[task].update(task_config[task])
     request_body = {"prompt": text, "config": config}
+
+    guardrails_triggered = []
 
     async with aiohttp.ClientSession() as session:
         async with session.post(
@@ -132,7 +139,6 @@ async def autoguard_infer(
                     f"AutoGuard call failed with status code {response.status}.\n"
                     f"Details: {await response.text()}"
                 )
-            guardrails_triggered = []
             async for line in response.content:
                 line_text = line.strip()
                 if len(line_text) > 0:
@@ -141,53 +147,7 @@ async def autoguard_infer(
                         guardrails_triggered.append(resp)
             if len(guardrails_triggered) > 0:
                 processed_response = process_autoguard_output(guardrails_triggered)
-                return True, processed_response[0], processed_response[1]
-    return False, None
-
-
-async def autoguard_pii_infer(
-    request_url: str,
-    text: str,
-    entities: List[str],
-    contextual_rules: List[List[str]],
-    matching_scores: Dict[str, Dict[str, float]],
-    task_config: Optional[Dict[Any, Any]] = None,
-):
-    """Provides request body for given text and other configuration"""
-    api_key = os.environ.get("AUTOGUARD_API_KEY")
-    if api_key is None:
-        raise ValueError("AUTOGUARD_API_KEY environment variable not set.")
-
-    headers = {"x-api-key": api_key}
-    config = DEFAULT_CONFIG
-    # enable the select guardrail
-    config["pii_fast"]["mode"] = "DETECT"
-    if task_config:
-        config["pii_fast"].update(task_config)
-
-    config["pii_fast"]["enabled_types"] = entities
-    config["pii_fast"]["contextual_rules"] = contextual_rules
-    if matching_scores:
-        config["pii_fast"]["matching_scores"] = matching_scores.get("pii_fast", {})
-    request_body = {"prompt": text, "config": config}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            url=request_url,
-            headers=headers,
-            json=request_body,
-        ) as response:
-            if response.status != 200:
-                raise ValueError(
-                    f"AutoGuard call failed with status code {response.status}.\n"
-                    f"Details: {await response.text()}"
-                )
-            async for line in response.content:
-                line_text = line.strip()
-                if len(line_text) > 0:
-                    resp = json.loads(line_text)
-                    if resp["task"] == "pii_fast":
-                        return resp["guarded"], resp["response"]
+                return [True] + processed_response
     return False, None
 
 
@@ -230,17 +190,15 @@ async def autoguard_input_api(
     """Calls AutoGuard API for the user message and guardrail configuration provided"""
     user_message = context.get("user_message")
     autoguard_config = llm_task_manager.config.rails.config.autoguard
-
     autoguard_api_url = autoguard_config.parameters.get("endpoint")
     if not autoguard_api_url:
         raise ValueError("Provide the autoguard endpoint in the config")
-    tasks = getattr(autoguard_config.input, "guardrails")
-    matching_scores = getattr(autoguard_config.input, "matching_scores", {})
-    if not tasks:
-        raise ValueError("Provide the guardrails in the config")
+    task_config = getattr(autoguard_config.input, "guardrails_config")
+    if not task_config:
+        raise ValueError("Provide the guardrails and their configuration")
     prompt = user_message
 
-    return await autoguard_infer(autoguard_api_url, prompt, tasks, matching_scores)
+    return await autoguard_infer(autoguard_api_url, prompt, task_config)
 
 
 @action(name="autoguard_output_api")
@@ -253,66 +211,47 @@ async def autoguard_output_api(
     autoguard_api_url = autoguard_config.parameters.get("endpoint")
     if not autoguard_api_url:
         raise ValueError("Provide the autoguard endpoint in the config")
-    tasks = getattr(autoguard_config.output, "guardrails")
-    matching_scores = getattr(autoguard_config.output, "matching_scores", {})
-    if not tasks:
-        raise ValueError("Provide the guardrails in the config")
+    task_config = getattr(autoguard_config.output, "guardrails_config")
+    if not task_config:
+        raise ValueError("Provide the guardrails and their configuration")
 
     prompt = bot_message
 
-    return await autoguard_infer(autoguard_api_url, prompt, tasks, matching_scores)
+    return await autoguard_infer(autoguard_api_url, prompt, task_config)
 
 
-@action(name="autoguard_pii_input_api")
-async def autoguard_pii_input_api(
+@action(name="autoguard_factcheck_input_api")
+async def autoguard_factcheck_input_api(
     llm_task_manager: LLMTaskManager, context: Optional[dict] = None
 ):
-    """Calls AutoGuard API for the user message and guardrail configuration provided"""
+    """Calls AutoGuard factcheck API and checks whether the user message is factually correct according to given
+    documents"""
+
     user_message = context.get("user_message")
+    documents = context.get("relevant_chunks", [])
     autoguard_config = llm_task_manager.config.rails.config.autoguard
-
-    autoguard_api_url = autoguard_config.parameters.get("endpoint")
-    if not autoguard_api_url:
-        raise ValueError("Provide the autoguard endpoint in the config")
-
-    entities = getattr(autoguard_config.input, "entities", [])
-    contextual_rules = getattr(autoguard_config.input, "contextual_rules", [])
-    matching_scores = getattr(autoguard_config.input, "matching_scores", {})
-    return await autoguard_pii_infer(
-        autoguard_api_url, user_message, entities, contextual_rules, matching_scores
+    autoguard_fact_check_api_url = autoguard_config.parameters.get(
+        "fact_check_endpoint"
     )
+    if not autoguard_fact_check_api_url:
+        raise ValueError("Provide the autoguard factcheck endpoint in the config")
+    if isinstance(documents, str):
+        documents = documents.split("\n")
+    prompt = user_message
+    if isinstance(documents, list) and len(documents) > 0:
+        return await autoguard_factcheck_infer(
+            autoguard_fact_check_api_url, prompt, documents
+        )
+    else:
+        raise ValueError("Provide relevant documents in proper format")
 
 
-@action(name="autoguard_pii_output_api")
-async def autoguard_pii_output_api(
-    llm_task_manager: LLMTaskManager, context: Optional[dict] = None
-):
-    """Calls AutoGuard API for the bot message and guardrail configuration provided"""
-    user_message = context.get("bot_message")
-    autoguard_config = llm_task_manager.config.rails.config.autoguard
-
-    autoguard_api_url = autoguard_config.parameters.get("endpoint")
-    if not autoguard_api_url:
-        raise ValueError("Provide the autoguard endpoint in the config")
-
-    entities = getattr(autoguard_config.output, "entities", [])
-    contextual_rules = getattr(autoguard_config.output, "contextual_rules", [])
-    matching_scores = getattr(autoguard_config.output, "matching_scores", {})
-    return await autoguard_pii_infer(
-        autoguard_api_url, user_message, entities, contextual_rules, matching_scores
-    )
-
-
-@action(name="autoguard_factcheck_api")
-async def autoguard_factcheck_api(
+@action(name="autoguard_factcheck_output_api")
+async def autoguard_factcheck_output_api(
     llm_task_manager: LLMTaskManager, context: Optional[dict] = None
 ):
     """Calls AutoGuard factcheck API and checks whether the bot message is factually correct according to given
     documents"""
-    api_key = os.environ.get("AUTOGUARD_API_KEY")
-
-    if api_key is None:
-        raise ValueError("AUTOGUARD_API_KEY environment variable not set.")
 
     bot_message = context.get("bot_message")
     documents = context.get("relevant_chunks", [])
@@ -331,3 +270,20 @@ async def autoguard_factcheck_api(
         )
     else:
         raise ValueError("Provide relevant documents in proper format")
+
+
+@action(name="autoguard_retrieve_relevant_chunks")
+async def autoguard_retrieve_relevant_chunks(
+    kb: Optional[KnowledgeBase] = None,
+):
+    """Retrieve knowledge chunks from knowledge base and update the context."""
+    context_updates = {}
+    chunks = [chunk["body"] for chunk in kb.chunks]
+
+    context_updates["relevant_chunks"] = "\n".join(chunks)
+    context_updates["relevant_chunks_sep"] = chunks
+
+    return ActionResult(
+        return_value=context_updates["relevant_chunks"],
+        context_updates=context_updates,
+    )
