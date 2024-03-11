e2169ef07d5484d563c080350f920b21f40a6257
dave <developer.dave@outlook.com>
2024-03-10 22:02:54 -0400

    312 Expose verbose arg in RunnableRails
    * Expose verbose arg in RunnableRails for LLMRails

--- tests/test_runnable_rails.py
+++ tests/test_runnable_rails.py
@@ -24,7 +24,6 @@ from langchain_core.runnables import (
     RunnableLambda,
     RunnablePassthrough,
 )
-from langchain_core.runnables.passthrough import RunnableAssign
 from langchain_core.runnables.utils import Input, Output
 
 from nemoguardrails import RailsConfig
@@ -42,6 +41,24 @@ def test_string_in_string_out():
     )
     config = RailsConfig.from_content(config={"models": []})
     model_with_rails = RunnableRails(config, llm=llm)
+
+    prompt = PromptTemplate.from_template("The capital of France is ")
+    chain = prompt | model_with_rails
+
+    result = chain.invoke(input={})
+
+    assert result == "Paris."
+
+
+def test_string_in_string_out_with_verbose_flag():
+    llm = FakeLLM(
+        responses=[
+            "Paris.",
+        ]
+    )
+    config = RailsConfig.from_content(config={"models": []})
+    model_with_rails = RunnableRails(config, llm=llm, verbose=True)
+    assert model_with_rails.rails.verbose is True
 
     prompt = PromptTemplate.from_template("The capital of France is ")
     chain = prompt | model_with_rails
