a461e0a864962b9a0249359ad9b4bdb879298578
Razvan Dinu <rdinu@nvidia.com>
2024-03-12 23:54:18 +0200

    Fix multiple issues with the grammar, including allowing support for operators in flow names.

--- tests/colang/parser/v2_x/test_syntax_parsing.py
+++ tests/colang/parser/v2_x/test_syntax_parsing.py
@@ -35,6 +35,8 @@ tests_root = os.path.join(os.path.dirname(__file__), "../../..")
         "colang/parser/v2_x/inputs/test8.co",
         "colang/parser/v2_x/inputs/test9.co",
         "colang/parser/v2_x/inputs/test10.co",
+        "colang/parser/v2_x/inputs/test11.co",
+        "colang/parser/v2_x/inputs/test12.co",
         "test_configs/example_flows_v_2_x/faq_questions.co",
         "test_configs/example_flows_v_2_x/core_flows.co",
         "test_configs/example_flows_v_2_x/confirmation_question.co",
