7bbc0a6097a9e171b228d24683f24ff46f367e3e
Razvan Dinu <rdinu@nvidia.com>
2024-04-22 13:27:58 +0300

    Fix `...` behavior when included in generated flow.

--- nemoguardrails/actions/v2_x/generation.py
+++ nemoguardrails/actions/v2_x/generation.py
@@ -16,10 +16,12 @@
 """A set of actions for generating various types of completions using an LLMs."""
 import logging
 import re
+import textwrap
 from ast import literal_eval
 from typing import Any, List, Optional
 
 from langchain.llms import BaseLLM
+from rich.text import Text
 
 from nemoguardrails.actions.actions import action
 from nemoguardrails.actions.llm.generation import LLMGenerationActions
@@ -47,6 +49,7 @@ from nemoguardrails.embeddings.index import EmbeddingsIndex, IndexItem
 from nemoguardrails.llm.filters import colang
 from nemoguardrails.llm.params import llm_params
 from nemoguardrails.llm.types import Task
+from nemoguardrails.utils import console
 
 log = logging.getLogger(__name__)
 
@@ -554,7 +557,12 @@ class LLMGenerationActionsV2dotx(LLMGenerationActions):
 
         flow_config = state.flow_configs[triggering_flow_id]
         docstrings = re.findall(r'"""(.*?)"""', flow_config.source_code, re.DOTALL)
-        assert len(docstrings) > 0
+
+        if len(docstrings) > 0:
+            docstring = docstrings[0]
+            self._last_docstring = docstring
+        else:
+            docstring = self._last_docstring
 
         render_context = {}
         render_context.update(state.context)
@@ -562,7 +570,7 @@ class LLMGenerationActionsV2dotx(LLMGenerationActions):
 
         # TODO: add the context of the flow
         prompt = self.llm_task_manager._render_string(
-            docstrings[0], context=render_context, events=events
+            textwrap.dedent(docstring), context=render_context, events=events
         )
 
         # We make this call with temperature 0 to have it as deterministic as possible.
