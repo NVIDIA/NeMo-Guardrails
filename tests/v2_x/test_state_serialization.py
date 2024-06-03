d022f63010133562ce29e615d1cd04655f65db3f
Christian Schüller <cschueller@nvidia.com>
2024-06-03 12:26:04 +0200

    Increase time threshold

--- tests/v2_x/test_state_serialization.py
+++ tests/v2_x/test_state_serialization.py
@@ -115,7 +115,7 @@ async def test_serialization():
         avg_time += took
     avg_time /= number_of_runs
 
-    assert avg_time < 0.01
+    assert avg_time < 0.02
 
     assert isinstance(s, str)
 
