ff2fc1fe5e1f6e468e014bb76df827d81b0b20df
Razvan Dinu <rdinu@nvidia.com>
2024-07-10 13:27:33 +0300

    Fix formatting issues.

--- nemoguardrails/llm/prompts.py
+++ nemoguardrails/llm/prompts.py
@@ -121,13 +121,13 @@ def get_prompt(config: RailsConfig, task: Union[str, Task]) -> TaskPrompt:
     task_model = "unknown"
     if config.models:
         _models = [model for model in config.models if model.type == task_name]
-        if not _models: 
+        if not _models:
             _models = [model for model in config.models if model.type == "main"]
 
-        task_model = _models[0].engine 
+        task_model = _models[0].engine
         if _models[0].model:
             task_model += "/" + _models[0].model
-            
+
     task_prompting_mode = "standard"
     if config.prompting_mode:
         # if exists in config, overwrite, else, default to "standard"
