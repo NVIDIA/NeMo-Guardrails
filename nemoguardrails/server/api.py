1337f98cad6289b47ee0139bfbc553659601a52b
prezakhani <13303554+Pouyanpi@users.noreply.github.com>
2024-07-11 12:03:50 +0200

    fix(api): remove unnecessary path check in _get_rails

--- nemoguardrails/server/api.py
+++ nemoguardrails/server/api.py
@@ -253,9 +253,6 @@ def _get_rails(config_ids: List[str]) -> LLMRails:
         base_path = os.path.abspath(app.rails_config_path)
         full_path = os.path.normpath(os.path.join(base_path, config_id))
 
-        if not full_path.startswith(base_path + os.sep):
-            raise ValueError("Access to the specified path is not allowed.")
-
         rails_config = RailsConfig.from_path(full_path)
 
         if not full_llm_rails_config:
