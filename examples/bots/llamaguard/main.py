9d66afe414a36193564f70d1f0e5781b4ee825a2
abodhankar <abodhankar@nvidia.com>
2024-08-30 15:27:10 -0700

    Adding license in the python scripts as comments

--- examples/bots/llamaguard/main.py
+++ examples/bots/llamaguard/main.py
@@ -1,7 +1,20 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
-
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
-
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.pydantic_v1 import BaseModel
 from nemoguardrails import RailsConfig, LLMRails
@@ -41,6 +54,7 @@ chain = (
 )
 
 chain_with_llamaguard = llamaguard | chain
+
 
 # Add typing for input
 class Question(BaseModel):
