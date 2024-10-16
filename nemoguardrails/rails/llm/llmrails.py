4be3307e27b0a10afeafeb907aa78fb950c7cd88
prezakhani <13303554+Pouyanpi@users.noreply.github.com>
2024-10-16 15:02:00 +0200

    fix: ensure correct response extraction in LLMRails to conform current api

--- nemoguardrails/rails/llm/llmrails.py
+++ nemoguardrails/rails/llm/llmrails.py
@@ -910,6 +910,7 @@ class LLMRails:
                     input=messages, response=res, adapters=self._log_adapters
                 )
                 await tracer.export_async()
+                res = res.response[0]
             return res
         else:
             # If a prompt is used, we only return the content of the message.
